c575f841f599fb45b34ebf4077d28bea6970feba
KingAmada <sarkiamada@gmail.com>
2026-03-14 19:18:02 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.DUESILY_CONFIG = {
+  supabaseUrl: 'https://wzjcdfyfkjkbblofohrr.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6amNkZnlma2prYmJsb2ZvaHJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MDEzODYsImV4cCI6MjA4OTA3NzM4Nn0.NItjbAPOcqELxTX4tuRlpUtWohs7CTDHGr-IhoeDyTo'
+};
