72a3308eb654aa4e20a1cbd81370b8e12447bc46
KingAmada <sarkiamada@gmail.com>
2026-03-16 06:53:35 +0100

    Add wixBackendBaseUrl to DUESILY_CONFIG

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.DUESILY_CONFIG = {
   supabaseUrl: 'https://wzjcdfyfkjkbblofohrr.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6amNkZnlma2prYmJsb2ZvaHJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MDEzODYsImV4cCI6MjA4OTA3NzM4Nn0.NItjbAPOcqELxTX4tuRlpUtWohs7CTDHGr-IhoeDyTo'
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6amNkZnlma2prYmJsb2ZvaHJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MDEzODYsImV4cCI6MjA4OTA3NzM4Nn0.NItjbAPOcqELxTX4tuRlpUtWohs7CTDHGr-IhoeDyTo',
+  wixBackendBaseUrl: 'https://peygo.net'
 };
